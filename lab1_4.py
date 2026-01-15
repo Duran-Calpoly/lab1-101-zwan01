3c6a2ffa4272a1626b8c854fe1db7f777129ddd5
Zwe Aung <91805108+zwan01@users.noreply.github.com>
2026-01-14 18:48:28 -0800

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,27 @@
-pass
+# Calc
+num1 = float(input("#1: "))
+num2 = float(input("#2: "))
+num3 = float(input("#3: "))
+
+def calculate_average(num1, num2, num3):
+  return (num1 + num2 + num3)/3 
+
+print(calculate_average(num1, num2, num3))
+
+# Tax
+
+num4 = float(input("Money: "))
+
+def bill_total(num4):
+    return (num4 * 0.1) + num4
+
+print(bill_total(num4))
+
+# Greet
+
+hello = input("What is your name? ")
+
+def name(hello):
+    return hello
+
+print(hello, ", hello.")
